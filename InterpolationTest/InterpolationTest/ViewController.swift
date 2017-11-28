ffbb38210879675425d657a144bc3dc469369b13
Hsin Miao <hsinmiao00@gmail.com>
2017-11-28 15:57:55 -0800

    swift lint

--- InterpolationTest/InterpolationTest/ViewController.swift
+++ InterpolationTest/InterpolationTest/ViewController.swift
@@ -9,20 +9,13 @@
 import UIKit
 import Accelerate
 
-struct PixelData {
-    var a: UInt8 = 0
-    var r: UInt8 = 0
-    var g: UInt8 = 0
-    var b: UInt8 = 0
-}
-
 class ViewController: UIViewController {
 	@IBOutlet var imageView: UIImageView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
- 
+
         let url1 = URL(string: "https://imgur.com/34d1Tef.jpg")!
         let url2 = URL(string: "https://imgur.com/uVACNxB.jpg")!
         let data1 = try! Data(contentsOf: url1)
@@ -89,21 +82,21 @@ class ViewController: UIViewController {
                 let len = Int(img1.size.height) * Int(img1.size.width) * 4
 
 				// Convert UnsafePointer<UInt8> to UnsafePointer<Float>
-				var pixelFloatAry1 = [Float](repeating : 0.0, count : len)
-				var pixelFloatAry2 = [Float](repeating : 0.0, count : len)
+				var pixelFloatAry1 = [Float](repeating: 0.0, count: len)
+				var pixelFloatAry2 = [Float](repeating: 0.0, count: len)
 				vDSP_vfltu8(pixelDataAry1, 1, &pixelFloatAry1, 1, vDSP_Length(len))
 				vDSP_vfltu8(pixelDataAry2, 1, &pixelFloatAry2, 1, vDSP_Length(len))
 
 				// Vector-Scalar Multiplication
-				var weightedPixelAry1 = [Float](repeating : 0.0, count : len)
-				var weightedPixelAry2 = [Float](repeating : 0.0, count : len)
+				var weightedPixelAry1 = [Float](repeating: 0.0, count: len)
+				var weightedPixelAry2 = [Float](repeating: 0.0, count: len)
 				var weight1 = ratio
 				var weight2 = 1 - ratio
 				vDSP_vsmul(&pixelFloatAry1, 1, &weight1, &weightedPixelAry1, 1, vDSP_Length(len))
 				vDSP_vsmul(&pixelFloatAry2, 1, &weight2, &weightedPixelAry2, 1, vDSP_Length(len))
 
 				// Vector Addition
-                var resultPixelFloatAry = [Float](repeating : 0.0, count : len)
+                var resultPixelFloatAry = [Float](repeating: 0.0, count: len)
                 vDSP_vadd(weightedPixelAry1, 1, weightedPixelAry2, 1, &resultPixelFloatAry, 1, vDSP_Length(len))
 
 				// Convert UnsafePointer<Float> to UnsafePointer<UInt8>
@@ -119,4 +112,3 @@ class ViewController: UIViewController {
 		return nil
     }
 }
-
